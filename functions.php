ed8f5f88c8696316a3a0167726287f714441aa7c
鼠子 <Tomori@miomoe.cn>
2025-07-21 15:44:57 +0800

    update frontend

--- functions.php
+++ functions.php
@@ -13,8 +13,13 @@ require_once __DIR__ . '/Core/TTDF.Config.php';
  * 主题自定义代码
  * theme custom code
  */
-// 输出 WelCome
-function WelCome()
-{
-    TTDF::Modules('WelCome');
-}
+/**
+ * 自定义function代码
+ * @example 下方写入主题的自定义代码
+ */
+TTDF_Hook::add_action('load_head', function () {
+    Get::Options('Custom_Code_Head', true);
+});
+TTDF_Hook::add_action('load_foot', function () {
+    Get::Options('Custom_Code_Foot', true);
+});
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 /**
  * 一个基于Vue3+Antdv构建的Typecho主题
  * @package Ritsu
@@ -7,4 +8,21 @@
  * @link https://github.com/ShuShuicu/Typecho-Theme-Development-Framework
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
-Get::Template('AppVue');
+?>
+<!DOCTYPE html>
+<html lang="zh-CN">
+
+<head>
+    <?php TTDF_Hook::do_action('load_head'); ?>
+    <link rel="icon" href="<?php get_options('FaviconUrl') ?>">
+    <script type="module" crossorigin src="<?php get_theme_file_url('Assets/index-bCl628V9.js?ver=') . get_theme_version(); ?>"></script>
+    <link rel="modulepreload" crossorigin href="<?php get_theme_file_url('Assets/vue-vendor-CJLHb3Yq.js?ver=') . get_theme_version(); ?>">
+    <link rel="stylesheet" crossorigin href="<?php get_theme_file_url('Assets/index-yIrybjE3.css?ver=') . get_theme_version(); ?>">
+</head>
+
+<body>
+    <div id="app"></div>
+    <?php TTDF_Hook::do_action('load_foot'); ?>
+</body>
+
+</html>
